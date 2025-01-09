94273173ad22812c415377b45f6fd2aa45c05318
Sellenite <714627528@qq.com>
2025-01-09 21:46:38 +0800

    引入WebOfficeSDK作为externals和type文件

--- src/modules/menu/menu2/menu2-1/index.vue
+++ src/modules/menu/menu2/menu2-1/index.vue
@@ -5,10 +5,12 @@
 </template>
 
 <script lang="ts">
+import WebOfficeSDK from 'wps';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
+    console.log(WebOfficeSDK)
   }
 })
 </script>
